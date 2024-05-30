a672033015005bbfc605999a42f3859686009657
bar rot <151166429+barrot9@users.noreply.github.com>
2024-05-30 15:32:17 +0300

    error control bug fixes

--- myset/myset.c
+++ myset/myset.c
@@ -11,6 +11,7 @@ Set SETA, SETB, SETC, SETD, SETE, SETF;
 
 /* Function prototypes */
 void turnOn(Set set, int bit);
+void turnOff(Set set, int bit);
 int isOn(Set set, int bit);
 void print_set(Set set);
 void union_set(Set result, Set set1, Set set2);
@@ -20,6 +21,8 @@ void symdiff_set(Set result, Set set1, Set set2);
 Set* getSetByName(char* name, char** undefinedSet);
 char* trimWhitespace(char* str);
 void read_set(char* args[], int num_args);
+void cmd_turnOn(char* args[], int num_args);
+void cmd_turnOff(char* args[], int num_args);
 void cmd_print_set(char* args[], int num_args);
 void cmd_union_set(char* args[], int num_args);
 void cmd_intersect_set(char* args[], int num_args);
@@ -28,9 +31,45 @@ void cmd_symdiff_set(char* args[], int num_args);
 void cmd_stop(char* args[], int num_args);
 void parseAndExecuteCommand(char* input);
 
+/* Command function prototypes */
+typedef void (*CommandFunc)(char* args[], int num_args);
+
+/* Command structure */
+typedef struct {
+    char* name;
+    CommandFunc func;
+    int expected_args;
+} Command;
+
+/* Commands array */
+Command commands[] = {
+    {"turnOn", cmd_turnOn, 2},
+    {"turnOff", cmd_turnOff, 2},
+    {"print_set", cmd_print_set, 1},
+    {"union_set", cmd_union_set, 3},
+    {"intersect_set", cmd_intersect_set, 3},
+    {"sub_set", cmd_sub_set, 3},
+    {"symdiff_set", cmd_symdiff_set, 3},
+    {"read_set", read_set, -1},  /* -1 indicates variable number of arguments */
+    {"stop", cmd_stop, 0},
+    {NULL, NULL, 0}  /* End of commands marker */
+};
+
+int running = 1;  /* Global variable to control the loop */
+
 /* Function to turn on a bit in a given set */
 void turnOn(Set set, int bit) {
     set[bit / 8] |= (1 << (bit % 8));
+}
+
+/* Function to turn off a bit in a given set */
+void turnOff(Set set, int bit) {
+    set[bit / 8] &= ~(1 << (bit % 8));
+}
+
+/* Function to check if a bit is on in a given set */
+int isOn(Set set, int bit) {
+    return set[bit / 8] & (1 << (bit % 8));
 }
 
 /* Function to print the numbers of the bits that are on in a set */
@@ -47,11 +86,6 @@ void print_set(Set set) {
         }
     }
     printf("\n");
-}
-
-/* Function to check if a bit is on in a given set */
-int isOn(Set set, int bit) {
-    return set[bit / 8] & (1 << (bit % 8));
 }
 
 /* Function to perform a union of two sets and store the result in a third set */
@@ -117,6 +151,76 @@ char* trimWhitespace(char* str) {
     return str;
 }
 
+/* Function to parse user input and call the appropriate command */
+void parseAndExecuteCommand(char* input) {
+    /* Normalize the input by removing extra spaces and handling commas properly */
+    char normalizedInput[256];
+    int j = 0;
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == ',' && (i == 0 || input[i - 1] == ' ' || input[i - 1] == '\t')) {
+            normalizedInput[j++] = ',';
+        } else if (input[i] == ',' && input[i + 1] == ' ') {
+            normalizedInput[j++] = ',';
+        } else if (input[i] == ' ' || input[i] == '\t') {
+            if (j > 0 && normalizedInput[j - 1] != ' ' && normalizedInput[j - 1] != ',') {
+                normalizedInput[j++] = ' ';
+            }
+        } else {
+            normalizedInput[j++] = input[i];
+        }
+    }
+    normalizedInput[j] = '\0';
+
+    /* Check for multiple consecutive commas */
+    char* p = strstr(normalizedInput, ",,");
+    if (p != NULL) {
+        printf("Error: Multiple consecutive commas\n");
+        return;
+    }
+
+    char* tokens[20];
+    int num_tokens = 0;
+    char* token;
+
+    token = strtok(normalizedInput, " ,");
+    while (token != NULL && num_tokens < 20) {
+        tokens[num_tokens++] = token;
+        token = strtok(NULL, " ,");
+    }
+    tokens[num_tokens] = NULL; /* Null-terminate the tokens array */
+
+    if (num_tokens == 0) {
+        printf("No command provided\n");
+        return;
+    }
+
+    /* Find and execute the command */
+    for (int i = 0; commands[i].name != NULL; i++) {
+        if (strcmp(tokens[0], commands[i].name) == 0) {
+            if (commands[i].expected_args != -1 && num_tokens - 1 < commands[i].expected_args) {
+                printf("Missing parameter\n");
+                return;
+            }
+            if (commands[i].expected_args != -1 && num_tokens - 1 > commands[i].expected_args) {
+                printf("Error: Extra text after end of command\n");
+                return;
+            }
+            /* Check for empty arguments (which result from consecutive commas) */
+            for (int k = 1; k < num_tokens; k++) {
+                if (strlen(trimWhitespace(tokens[k])) == 0) {
+                    printf("Error: Multiple consecutive commas\n");
+                    return;
+                }
+            }
+            commands[i].func(tokens + 1, num_tokens - 1); /* Pass the arguments to the command function */
+            return;
+        }
+    }
+
+    printf("Undefined command name: %s\n", tokens[0]);
+}
+
 /* Function to read and set bits in a set */
 void read_set(char* args[], int num_args) {
     Set* set;
@@ -124,6 +228,14 @@ void read_set(char* args[], int num_args) {
     int end_with_minus_one;
     int i, j;
     char* undefinedSet = NULL;
+
+    /* Check for multiple consecutive commas */
+    for (i = 1; i < num_args; i++) {
+        if (strlen(trimWhitespace(args[i])) == 0) {
+            printf("Error: Multiple consecutive commas\n");
+            return;
+        }
+    }
 
     set = getSetByName(trimWhitespace(args[0]), &undefinedSet);
     if (set == NULL) {
@@ -134,11 +246,6 @@ void read_set(char* args[], int num_args) {
     end_with_minus_one = 0;
     for (i = 1; i < num_args; i++) {
         char* trimmedArg = trimWhitespace(args[i]);
-        /* Check for empty argument */
-        if (strlen(trimmedArg) == 0) {
-            printf("Error: Empty argument\n");
-            return;
-        }
         /* Convert argument to integer */
         char *endptr;
         bit = strtol(trimmedArg, &endptr, 10);
@@ -152,7 +259,7 @@ void read_set(char* args[], int num_args) {
             /* Check for any non-whitespace characters after -1 */
             for (j = i + 1; j < num_args; j++) {
                 if (strlen(trimWhitespace(args[j])) != 0) {
-                    printf("Error: Extra text after ending value -1\n");
+                    printf("Error: Extra text after end of command\n");
                     return;
                 }
             }
@@ -170,37 +277,37 @@ void read_set(char* args[], int num_args) {
     }
 }
 
-/* Command function prototypes */
-typedef void (*CommandFunc)(char* args[], int num_args);
-
-void cmd_print_set(char* args[], int num_args);
-void cmd_union_set(char* args[], int num_args);
-void cmd_intersect_set(char* args[], int num_args);
-void cmd_sub_set(char* args[], int num_args);
-void cmd_symdiff_set(char* args[], int num_args);
-void cmd_stop(char* args[], int num_args);
-
-/* Command structure */
-typedef struct {
-    char* name;
-    CommandFunc func;
-    int expected_args;
-} Command;
-
-Command commands[] = {
-    {"print_set", cmd_print_set, 1},
-    {"union_set", cmd_union_set, 3},
-    {"intersect_set", cmd_intersect_set, 3},
-    {"sub_set", cmd_sub_set, 3},
-    {"symdiff_set", cmd_symdiff_set, 3},
-    {"read_set", read_set, -1},  /* -1 indicates variable number of arguments */
-    {"stop", cmd_stop, 0},
-    {NULL, NULL, 0}  /* End of commands marker */
-};
-
-int running = 1;  /* Global variable to control the loop */
-
 /* Command function implementations */
+void cmd_turnOn(char* args[], int num_args) {
+    Set* set;
+    int bit;
+    char* undefinedSet = NULL;
+
+    set = getSetByName(trimWhitespace(args[0]), &undefinedSet);
+    if (set == NULL) {
+        printf("Undefined set name: %s\n", undefinedSet);
+        return;
+    }
+
+    bit = strtol(trimWhitespace(args[1]), NULL, 10);
+    turnOn(*set, bit);
+}
+
+void cmd_turnOff(char* args[], int num_args) {
+    Set* set;
+    int bit;
+    char* undefinedSet = NULL;
+
+    set = getSetByName(trimWhitespace(args[0]), &undefinedSet);
+    if (set == NULL) {
+        printf("Undefined set name: %s\n", undefinedSet);
+        return;
+    }
+
+    bit = strtol(trimWhitespace(args[1]), NULL, 10);
+    turnOff(*set, bit);
+}
+
 void cmd_print_set(char* args[], int num_args) {
     Set* set;
     char* undefinedSet = NULL;
@@ -317,40 +424,6 @@ void cmd_symdiff_set(char* args[], int num_args) {
 
 void cmd_stop(char* args[], int num_args) {
     running = 0;  /* Set the running flag to 0 to stop the loop */
-}
-
-/* Function to parse user input and call the appropriate command */
-void parseAndExecuteCommand(char* input) {
-    char* tokens[20];
-    int num_tokens = 0;
-    char* token;
-    int i;
-
-    token = strtok(input, " ,");
-    while (token != NULL && num_tokens < 20) {
-        tokens[num_tokens++] = token;
-        token = strtok(NULL, " ,");
-    }
-    tokens[num_tokens] = NULL; /* Null-terminate the tokens array */
-
-    if (num_tokens == 0) {
-        printf("No command provided\n");
-        return;
-    }
-
-    /* Find and execute the command */
-    for (i = 0; commands[i].name != NULL; i++) {
-        if (strcmp(tokens[0], commands[i].name) == 0) {
-            if (commands[i].expected_args != -1 && num_tokens - 1 != commands[i].expected_args) {
-                printf("Error: Extra text after end of command\n");
-                return;
-            }
-            commands[i].func(tokens + 1, num_tokens - 1); /* Pass the arguments to the command function */
-            return;
-        }
-    }
-
-    printf("Undefined command name: %s\n", tokens[0]);
 }
 
 int main() {
